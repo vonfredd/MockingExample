049cd1d805f7eabedeb915a55d765b572dea85cf
Fredrik Lundsten <f.lundsten@hotmail.se>
2024-01-18 18:51:12 +0100

    Removed custom implementation

--- src/test/java/com/example/BankServiceImpl.java
+++ src/test/java/com/example/BankServiceImpl.java
@@ -1,9 +1,7 @@
 package com.example;
 
 public class BankServiceImpl implements BankService {
-    public boolean payWasCalled = false;
     @Override
     public void pay(String id, double amount) {
-        payWasCalled = true;
     }
 }
