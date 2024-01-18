9fcd84a281f5b180d9555336da8706d7d91b8edc
Fredrik Lundsten <f.lundsten@hotmail.se>
2024-01-18 10:08:59 +0100

    Add BeforeEach for the mock

--- src/test/java/com/example/EmployeeTest.java
+++ src/test/java/com/example/EmployeeTest.java
@@ -1,10 +1,18 @@
 package com.example;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmployeeTest {
+
+    @BeforeEach
+    void setUp(){
+    }
 
     @Test
     void getId() {
