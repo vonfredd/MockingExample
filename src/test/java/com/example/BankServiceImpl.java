4056f3f508bb00632519a06326c568a2e12d66b9
Fredrik Lundsten <f.lundsten@hotmail.se>
2024-01-18 18:30:03 +0100

    Update BankServiceImpl to a spy with a call controll flag

--- src/test/java/com/example/BankServiceImpl.java
+++ src/test/java/com/example/BankServiceImpl.java
@@ -1,8 +1,9 @@
 package com.example;
 
 public class BankServiceImpl implements BankService {
+    public boolean payWasCalled = false;
     @Override
     public void pay(String id, double amount) {
-
+        payWasCalled = true;
     }
 }
