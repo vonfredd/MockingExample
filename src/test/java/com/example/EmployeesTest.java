108a238b2ee297c2a28dd085f43bb04ff2361632
Fredrik Lundsten <f.lundsten@hotmail.se>
2024-01-18 18:01:21 +0100

    Add a mocked list in setUp and instantiate the Employees object in payEmployees method

--- src/test/java/com/example/EmployeesTest.java
+++ src/test/java/com/example/EmployeesTest.java
@@ -1,10 +1,15 @@
 package com.example;
 
+import jdk.jfr.Description;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 class EmployeesTest {
     private EmployeeRepository employeeRepository;
@@ -12,11 +17,19 @@ class EmployeesTest {
 
     @BeforeEach
     void setUp(){
-        employeeRepository = Mockito.mock(EmployeeRepository.class);
-        bankService = Mockito.mock(BankService.class);
+        employeeRepository = mock(EmployeeRepository.class);
+        bankService = new BankServiceImpl();
+        when(employeeRepository.findAll()).thenReturn(List.of(
+                new Employee("100", 10_000.0),
+                new Employee("101", 20_000.0),
+                new Employee("102", 12_000.0),
+                new Employee("103", 15_000.0)));
     }
 
     @Test
+    @Description("Given")
     void payEmployees() {
+        Employees employees = new Employees(employeeRepository,bankService);
+
     }
 }
