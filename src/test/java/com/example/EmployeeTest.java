6ab1411cb41acc2c80a97598c404c3064512ee53
Fredrik Lundsten <f.lundsten@hotmail.se>
2024-01-18 10:03:47 +0100

    Add EmployeeTest

---
+++ src/test/java/com/example/EmployeeTest.java
@@ -0,0 +1,36 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void getSalary() {
+    }
+
+    @Test
+    void setSalary() {
+    }
+
+    @Test
+    void isPaid() {
+    }
+
+    @Test
+    void setPaid() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
