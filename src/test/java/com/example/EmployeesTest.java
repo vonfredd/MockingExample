4ad9c854ded734d95c55940ac03d2644dfc528e9
Fredrik Lundsten <f.lundsten@hotmail.se>
2024-01-18 17:38:51 +0100

    Add setUp method that instantiates mock of EmployeeRepository and BankService

--- src/test/java/com/example/EmployeesTest.java
+++ src/test/java/com/example/EmployeesTest.java
@@ -1,10 +1,20 @@
 package com.example;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmployeesTest {
+    private EmployeeRepository employeeRepository;
+    private BankService bankService;
+
+    @BeforeEach
+    void setUp(){
+        employeeRepository = Mockito.mock(EmployeeRepository.class);
+        bankService = Mockito.mock(BankService.class);
+    }
 
     @Test
     void payEmployees() {
