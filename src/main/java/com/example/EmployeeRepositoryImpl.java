dae5255fd955c2523d139de711eccf84036f774b
Fredrik Lundsten <f.lundsten@hotmail.se>
2024-01-23 13:09:07 +0100

    Add employeeList as field to save employees. Edit findAll() to return a copy of the current employeeList

--- src/main/java/com/example/EmployeeRepositoryImpl.java
+++ src/main/java/com/example/EmployeeRepositoryImpl.java
@@ -1,11 +1,14 @@
 package com.example;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class EmployeeRepositoryImpl implements EmployeeRepository{
+
+    private final List<Employee> employeeList = new ArrayList<>();
     @Override
     public List<Employee> findAll() {
-        return null;
+        return List.copyOf(employeeList);
     }
 
     @Override
