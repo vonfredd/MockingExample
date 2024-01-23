ce5a6044eefd7540c80ae48ea63f9e3998e6d35d
Fredrik Lundsten <f.lundsten@hotmail.se>
2024-01-23 14:24:30 +0100

    Add id field that will increment with each new employee added. Update save() that will check if id exist or is null

--- src/main/java/com/example/EmployeeRepositoryImpl.java
+++ src/main/java/com/example/EmployeeRepositoryImpl.java
@@ -2,10 +2,15 @@ package com.example;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
-public class EmployeeRepositoryImpl implements EmployeeRepository{
+public class EmployeeRepositoryImpl implements EmployeeRepository {
 
     private final List<Employee> employeeList = new ArrayList<>();
+    private static int latestId = 100;
+
+
     @Override
     public List<Employee> findAll() {
         return List.copyOf(employeeList);
@@ -13,6 +18,23 @@ public class EmployeeRepositoryImpl implements EmployeeRepository{
 
     @Override
     public Employee save(Employee e) {
+        if (e.getId() == null) {
+            e.setId(String.valueOf(++latestId));
+            employeeList.add(e);
+            return e;
+        }
+
+        boolean isFound = false;
+
+        for (int i = 0; i < employeeList.size(); i++) {
+            if (e.getId().equals(employeeList.get(i).getId())) {
+                employeeList.set(i, e);
+                isFound = true;
+                break;
+            }
+        }
+        if (!isFound)
+            employeeList.add(e);
         return e;
     }
 }
