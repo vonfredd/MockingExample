830ea58364a84eaa0791523357e536dd932bfb6f
Fredrik Lundsten <f.lundsten@hotmail.se>
2024-01-23 14:39:07 +0100

    Add employeeRepositoryImpl test

---
+++ src/test/java/com/example/EmployeeRepositoryImplTest.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeRepositoryImplTest {
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void save() {
+    }
+}
