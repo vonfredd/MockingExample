b9d936b1965a2c4589934a5a013c2058cc523514
Fredrik Lundsten <f.lundsten@hotmail.se>
2024-01-25 11:09:30 +0100

    Empty String should return 0

--- src/main/java/com/classes/Calculator.java
+++ src/main/java/com/classes/Calculator.java
@@ -1,2 +1,9 @@
-package com.classes;public class Calculator {
+package com.classes;
+
+import java.util.stream.Stream;
+
+public class Calculator {
+    public int add(String input) {
+        return input.isEmpty() ? 0 : Stream.of(input.split(",")).mapToInt(Integer::parseInt).sum();
+    }
 }
--- src/test/java/com/classes/CalculatorTest.java
+++ src/test/java/com/classes/CalculatorTest.java
@@ -1,4 +1,30 @@
+package com.classes;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
 class CalculatorTest {
-  
+    Calculator calculator = new Calculator();
+
+    @ParameterizedTest
+    @DisplayName("Returns sum of two numbers")
+    @CsvSource (value = {"1,2"}, delimiter = (';'))
+    void returnsSumOfTwoNumbers(String input){
+        int sum = calculator.add(input);
+        assertThat(sum).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("Empty string should return 0")
+    void emptyStringReturns0(){
+        int sum = calculator.add("");
+        assertThat(sum).isEqualTo(0);
+    }
 }
