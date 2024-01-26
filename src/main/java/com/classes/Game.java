1126b61515938c43ecc6afe1564ee9a6c59fc964
Fredrik Lundsten <f.lundsten@hotmail.se>
2024-01-26 21:54:32 +0100

    Add test and to check returned score

--- src/main/java/com/classes/Game.java
+++ src/main/java/com/classes/Game.java
@@ -1,5 +1,13 @@
 package com.classes;
 
 public class Game {
+    private int score;
 
+    public void roll(int i) {
+         score = i;
+    }
+
+    public int score() {
+        return score;
+    }
 }
--- src/test/java/com/classes/GameTest.java
+++ src/test/java/com/classes/GameTest.java
@@ -1,7 +1,20 @@
 package com.classes;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
+    Game game = new Game();
+
+    @Test
+    @DisplayName("Score should return 5 if 5 pins are knocked down")
+    void scoreShouldReturn5If5PinsAreKnockedDown(){
+        game.roll(5);
+        int score = game.score();
+        assertThat(score).isEqualTo(5);
+    }
 
 }
