ffb6e7e7bb4ad19d6ac81b6156af132e16c2c1a3
Fredrik Lundsten <f.lundsten@hotmail.se>
2024-01-28 19:36:13 +0100

    Add test to check while loop in score() so that it does not add more points when game is over. Kill mutation

--- src/test/java/com/classes/GameTest.java
+++ src/test/java/com/classes/GameTest.java
@@ -2,28 +2,23 @@ package com.classes;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
     Game game = new Game();
 
     @Test
     @DisplayName("Score should return 5 if 5 pins are knocked down")
-    void scoreShouldReturn5If5PinsAreKnockedDown(){
+    void scoreShouldReturn5If5PinsAreKnockedDown() {
         game.roll(5);
         int score = game.score();
         assertThat(score).isEqualTo(5);
     }
-    
+
     @Test
     @DisplayName("Should return sum of both rolls if two rolls have been played")
-    void shouldReturnSumIfTwoRollsHaveBeenPlayed(){
+    void shouldReturnSumIfTwoRollsHaveBeenPlayed() {
         game.roll(2);
         game.roll(5);
         assertThat(game.score()).isEqualTo(7);
@@ -31,24 +26,24 @@ class GameTest {
 
     @Test
     @DisplayName("Should return true if result is 17")
-    void shouldReturnTrueIfResultIs17(){
+    void shouldReturnTrueIfResultIs17() {
         game.roll(5);
         game.roll(5);
         game.roll(1);
         game.roll(5);
         assertThat(game.score()).isEqualTo(17);
     }
-    
+
     @Test
     @DisplayName("Returns 1 if player has rolled")
-    void returns1IfCurrentRollIsFirstroll(){
+    void returns1IfCurrentRollIsFirstroll() {
         game.roll(5);
-        assertEquals(game.rollSquares.size(),1);
+        assertEquals(1, game.rollSquares.size());
     }
-    
+
     @Test
     @DisplayName("Given a set of rounds, strike should be added accordingly")
-    void givenASetOfRoundsStrikeShouldBeAddedAccordingly(){
+    void givenASetOfRoundsStrikeShouldBeAddedAccordingly() {
         game.roll(4);
         game.roll(3);
         game.roll(10);
@@ -58,4 +53,24 @@ class GameTest {
         assertThat(game.score()).isEqualTo(35);
     }
 
+
+    @Test
+    @DisplayName("Given 13 strikes, score() should only calculate 12 of them since that is max fields")
+    void given13StrikesScoreShouldOnlyCalculate12OfThemSinceThatIsMaxFields(){
+        game.roll(10);
+        game.roll(10);
+        game.roll(10);
+        game.roll(10);
+        game.roll(10);
+        game.roll(10);
+        game.roll(10);
+        game.roll(10);
+        game.roll(10);
+        game.roll(10);
+        game.roll(10);
+        game.roll(10);
+        game.roll(10);
+
+        assertThat(game.score()).isEqualTo(300);
+    }
 }
